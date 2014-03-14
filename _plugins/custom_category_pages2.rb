6a09063cb6c9049d8b1a751ab651b8b46f4e7283
anna mendoza <anna@curvecommunications.biz>
2014-03-14 13:51:37 -0700

    updates to understand this structure

--- _plugins/custom_category_pages2.rb
+++ _plugins/custom_category_pages2.rb
@@ -1,13 +1,13 @@
-def write_category_indexes
-  
-  if self.layouts.key? 'tutorials_category_index'
-    self.categories.keys.each do |category|
-      next if RESTRICTED_CATEGORIES.include? category
-      self.write_category_index(category)
-    end
+# def write_category_indexes
+#   
+#   if self.layouts.key? 'tutorials_category_index'
+#     self.categories.keys.each do |category|
+#       next if RESTRICTED_CATEGORIES.include? category
+#       self.write_category_index(category)
+#     end  
  
   # Throw an exception if the layout couldn't be found.
-  else
-    throw "No 'category_index' layout found."
-  end
-end
+#   else
+#     throw "No 'category_index' layout found."
+#   end
+# end 
